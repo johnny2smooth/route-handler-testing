dfabf76cd4cf7cd17f23e4f7895566f4c2e5298a
johnny2smooth <johnbp13@gmail.com>
2023-03-29 14:56:45 -0700

    starting to figure it out

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,21 @@
+import { createClient } from "@supabase/supabase-js";
+import { NextResponse } from "next/server";
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+  const { data, error } = await supabase.storage.from("friend-drawings").list();
+  let URL = data?.map(
+    (item) =>
+      `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/friend-drawings/${item.name}`
+  );
+  return NextResponse.json(URL);
+}
+
+export async function POST(request: Request) {
+  const { prompt } = await request.json();
+  console.log(prompt);
+  return NextResponse.json(prompt);
 }
